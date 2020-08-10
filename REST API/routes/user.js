45b193e35d371702209c007328ac62dc3438f1dd
Kristian Kazalev <kristian.kazalev@gmail.com>
2020-08-10 08:48:04 +0300

    feat: profile page

--- REST API/routes/user.js
+++ REST API/routes/user.js
@@ -1,6 +1,8 @@
 const controllers = require('../controllers/');
 const router = require('express').Router();
 
+router.get('/', controllers.user.get);
+
 router.post('/register', controllers.user.post.register);
 
 router.post('/login', controllers.user.post.login);
--- src/navigation.js
+++ src/navigation.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './components/home/home'
 import Register from './pages/register'
 import Login from './pages/login'
+import ProfilePage from './pages/profile'
 
 const Navigation = () => {
 
@@ -10,8 +11,9 @@ const Navigation = () => {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
-                <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+                <Route path="/profile/:userID" component={ProfilePage} />
             </Switch>
         </BrowserRouter>
     )
